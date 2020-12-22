2e2982f464a6f43b4e668d6994154c3226c39fb3
HardlyCodeMan <graham@planbit.solutions>
2020-12-22 16:48:35 +0800

    Add: Ensure non-owners cannot withdrawAll()

--- test/valuetests.js
+++ test/valuetests.js
@@ -37,29 +37,33 @@ contract("People", async function(accounts) {
 
     it("Check contract balances are increased correctly when creating a person.", async function() {
         contractPreBalance = await web3.eth.getBalance(instance.address);
-        console.log(" > > Balance pre creation: " + contractPreBalance);
+        //console.log(" > > Balance pre creation: " + contractPreBalance);
+        
         await instance.createPerson("Person 1", 35, 170, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
         contractPostBalance = await web3.eth.getBalance(instance.address);
-        console.log(" > > Balance post creation: " + contractPostBalance);
+        //console.log(" > > Balance post creation: " + contractPostBalance);
 
         await truffleAssert.passes(contractPostBalance === contractPreBalance + web3.utils.toWei("1", "ether"));
         await truffleAssert.passes(instance.balance === contractPostBalance);
+    });
+    it("Ensure non-owners cannot withdrawAll().", async function() {
+        await truffleAssert.fails(instance.withdrawAll({from: accounts[1]}), truffleAssert.ErrorType.REVERT);
     });
     it("Check contract balance is zeroed and owner balance is increased correctly when withdrawing.", async function() {
         contractPreBalance = await web3.eth.getBalance(instance.address);
         ownerPreWithdrawl = await web3.eth.getBalance(accounts[0]);
 
-        console.log(" > > Contract balance pre creation: " + contractPreBalance);
-        console.log(" > > Owner balance pre creation: " + ownerPreWithdrawl);
+        //console.log(" > > Contract balance pre creation: " + contractPreBalance);
+        //console.log(" > > Owner balance pre creation: " + ownerPreWithdrawl);
 
         await instance.withdrawAll({from: accounts[0]});
         contractPostBalance = await web3.eth.getBalance(instance.address);
         ownerPostWithdrawl = await web3.eth.getBalance(accounts[0]);
         
-        console.log(" > > Contract balance post withdrawl: " + contractPostBalance);
-        console.log(" > > Owner balance post withdrawl: " + ownerPostWithdrawl);
+        //console.log(" > > Contract balance post withdrawl: " + contractPostBalance);
+        //console.log(" > > Owner balance post withdrawl: " + ownerPostWithdrawl);
 
         await truffleAssert.passes(contractPostBalance === 0);
-        await truffleAssert.passes(ownerPostWithdrawl === ownerPreWithdrawl + contractPreBalance);
+        await truffleAssert.passes(ownerPostWithdrawl > ownerPreWithdrawl);
     });
 });
