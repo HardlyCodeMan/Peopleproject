957fa8a6e604926123624f6c6b580280cf526fc1
HardlyCodeMan <graham@planbit.solutions>
2020-12-22 15:19:22 +0800

    Add: Testing onlyOwner modifier

---
+++ test/ownershiptests.js
@@ -0,0 +1,33 @@
+// Unit testing assignment
+
+// requirement: truffle-assertions
+// install: npm install truffle-assertions
+// usage: const truffleAssert = require('truffle-assertions');
+
+// Tests:
+// Check onlyOwner works correctly
+// Check person deletion works correctly
+
+// Ownable ensures require(msg.sender == owner);
+// Functions using onlyOwner:
+// function deletePerson(address creator) public onlyOwner {
+// function getCreator(uint index) public view onlyOwner returns(address){
+// function withdrawAll() public onlyOwner returns(uint) {
+
+const People = artifacts.require("People");
+const truffleAssert = require('truffle-assertions');
+
+contract("People", async function(accounts) {
+    it("Non-owner should not be able to delete people", async function() {
+        let instance = await People.deployed();
+        
+        await instance.createPerson("Person 1", 35, 170, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
+        await truffleAssert.fails(instance.deletePerson(accounts[1], {from: accounts[1]}), truffleAssert.ErrorType.REVERT);
+    });
+    it("Owner should be able to delete people", async function() {
+        let instance = await People.deployed();
+        
+        await instance.createPerson("Person 2", 35, 170, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
+        await truffleAssert.passes(instance.deletePerson(accounts[2], {from: accounts[0]}));
+    });
+});
